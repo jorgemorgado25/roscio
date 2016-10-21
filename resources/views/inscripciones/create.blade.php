c3b17566087ff383889173a7eed76b861918e04a
jorgemorgado25 <jorgemorgado25@gmail.com>
2016-10-20 21:55:23 -0400

    Pruebas con vue en inscripciones.create

--- resources/views/inscripciones/create.blade.php
+++ resources/views/inscripciones/create.blade.php
@@ -6,8 +6,8 @@
 <div class="col-md-6 col-md-offset-3">
 <h3 class="text-center">Realizar Inscripción</h3><br>
 @include('partials.error-message')
-<div class="box box-primary">
-	<form action="{{ route('inscripciones.store') }}" method="POST" id="form-create">
+<div class="box box-primary" id="app">
+	<form method="POST" id="form-create">
 
 	<div class="box-header with-border">
 		<h3 class="box-title">Datos del Estudiante</h3>
@@ -18,7 +18,7 @@
 			<div class="col-md-12">
 				<div class="form-group">
 					<label for="">Cédula del Estudiante</label>
-					<input value="{{ $cedula }}" id="txt-cedula-estudiante" name="cedula" type="text" class="form-control text-center input-lg" required>
+					<input value="{{ $cedula }}" id="txt-cedula-estudiante" name="cedula" type="text" class="form-control text-center input-lg">
 				</div>
 			</div>
 		</div>
@@ -27,7 +27,7 @@
 			<p>Estudiante no registrado</p>
 		</div>
 
-			<div id="div-spinner" class="panel text-center">
+			<div v-if="buscando" id="div-spinner" class="panel text-center">
 				<i class="fa fa-spinner fa-spin"></i> Buscando
 			</div>
 
@@ -69,26 +69,35 @@
 			<div class="col-md-6">
 				<div class="form-group">
 					<label for="">Mención</label>
-					{!! Form::select('mencion_id', $menciones, NULL, ['class' => 'form-control', 'placeholder' => '', 'required' => 'required', 'id' => 'sel-mencion']) !!}
+					{!! Form::select('mencion_id', $menciones, NULL, ['class' => 'form-control', 'placeholder' => '', 'required' => 'required', 'id' => 'sel-mencion', 'v-model' => 'mencion_id']) !!}
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
 					<label for="">Ano</label>
-					<select name="ano_id" id="sel_ano" class="form-control" required></select>
+					<select name="ano_id" id="sel_ano" class="form-control" v-model="ano_id">
+						
+					</select>
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
 					<label for="">Sección</label>
-					<select name="seccion_id" id="sel_seccion" class="form-control" placeholder="Selccione una seccion" required></select>
+					<select name="seccion_id" 
+						id="sel_seccion" 
+						class="form-control" 
+						placeholder="Selccione una seccion" 
+						v-model="seccion_id"
+						>
+						<option v-for="seccion in secciones" :value="option.value">@{{ seccion.text }}</option>
+					</select>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div class="box-footer clearfix">
-		{{ csrf_field() }}
+
 		<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> &nbsp;Inscribir Estudiante</button>
 	</div>
 	</form>
@@ -97,11 +106,38 @@
 @endsection
 
 @section('scripts')
-	<script src="{{ asset('/js/inscripciones-create.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/buscar-estudiante-ci.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/buscar-persona-ci.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/buscar-persona-id.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/buscar-anos.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/buscar-secciones.js') }}" type="text/javascript"></script>
-	<script src="{{ asset('/js/mencion-ano-seccion.js') }}" type="text/javascript"></script>
+
+<script src="{{ asset('/js/mixin.js') }}"></script>
+<script>
+
+var Component = Vue.extend({
+  mixins: [myMixin]
+});
+
+var component = new Component();
+component.hello();
+
+	vm = new Vue({
+		el: 'body',
+		data: {
+			mencion_id: '',
+			ano_id: '',
+			seccion_id: '',
+			anos: [],
+			/*secciones: [
+				{text: 'One', value: '1'},
+				{text: 'Two', value: '2'},
+				{text: 'Three', value: '3'}
+			],*/			
+			secciones: [],
+			buscando: false
+		},
+		methods: {
+
+		},
+		resource: {
+
+		}
+	});
+</script>
 @endsection
