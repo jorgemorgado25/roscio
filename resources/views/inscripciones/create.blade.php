944c45639c9f3db99ad084823d51fd942d07c077
jorgemorgado25 <jorgemorgado25@gmail.com>
2016-10-23 22:29:05 -0400

    update: incripciones.create

--- resources/views/inscripciones/create.blade.php
+++ resources/views/inscripciones/create.blade.php
@@ -3,125 +3,136 @@
 	Realizar Inscripción
 @endsection
 @section('main-content')
-<div class="col-md-6 col-md-offset-3">
-<h3 class="text-center">Realizar Inscripción</h3><br>
-@include('partials.error-message')
-<div class="box box-primary" id="app">
-	<form method="POST" id="form-create" action="{{ route('inscripciones.store') }}">
-	<div class="box-header with-border">
-		<h3 class="box-title">Datos del Estudiante</h3>
-		
-	</div>
-	<div class="box-body">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="form-group">
-					<label for="">Cédula del Estudiante</label>
-					<input value="{{ $cedula }}" 
-					id="txt-cedula-estudiante" 
-					name="cedula" 
-					type="text" 
-					v-model="estudiante.cedula" 
-					@focusout="buscarEst()"
-					class="form-control text-center input-lg">
+
+<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+		<h3 class="text-center">Realizar Inscripción</h3><br>
+		@include('partials.error-message')
+		<div class="box box-primary" id="app">
+			<form method="POST" id="form-create" action="{{ route('inscripciones.store') }}">
+			<div class="box-header with-border">
+				<h3 class="box-title">Datos del Estudiante</h3>
+				
+			</div>
+			<div class="box-body">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<label for="">Cédula del Estudiante</label>
+							<input value="{{ $cedula }}" 
+							id="txt-cedula-estudiante" 
+							name="cedula" 
+							type="text" 
+							v-model="estudiante.cedula" 
+							class="form-control text-center input-lg">
+							<br>
+
+							<div v-if="error" class="text-center alert alert-danger" id="div-alert">
+					<p>@{{ error }}</p>
 				</div>
-			</div>
-		</div>
 
-		<div v-if="error" class="text-center alert alert-danger" id="div-alert">
-			<p>@{{ error }}</p>
-		</div>
-
-			<div v-if="buscando" id="div-spinner" class="panel text-center">
-				<i class="fa fa-spinner fa-spin"></i> Buscando
-			</div>
-
-		<div class="row" v-if="estudiante.nombre">
-			<div class="col-md-6" id="div-nombre-estudiante">
-				<div class="box box-success">
-					<div class="box-body">
-						<label for="">Nombre y Apellido</label>
-						<p id="p-nombre-estudiante">@{{ estudiante.nombre }} @{{ estudiante.apellido }}</p>
+							<button type="button" class="btn btn-primary btn-lg" @click="buscarEst()" style="width:100%">
+								Buscar Estudiante
+							</button>
+						</div>
 					</div>
-				</div>				
+				</div>
+
+				
+					<div v-if="buscando" id="div-spinner" class="panel text-center">
+						<i class="fa fa-spinner fa-spin"></i> Buscando
+					</div>
+
+				<div class="row" v-if="estudiante.nombre">
+					<div class="col-md-6" id="div-nombre-estudiante">
+						<div class="box box-success">
+							<div class="box-body">
+								<label for="">Nombre y Apellido</label>
+								<p id="p-nombre-estudiante">@{{ estudiante.nombre }} @{{ estudiante.apellido }}</p>
+							</div>
+						</div>				
+					</div>
+
+					<div class="col-md-6" id="div-nombre-representante">
+						<div class="box box-danger">
+							<div class="box-body">
+							<a href="#" title="Actualizar Representante" class="btn btn-sm btn-default pull-right"><span class="glyphicon glyphicon-refresh"></span></a>
+								<label for="">Representante </label>
+								<p id="p-nombre-representante">@{{ representante.nombre }} @{{ representante.apellido }}</p>
+
+							</div>					
+						</div>
+					</div>
+				</div>
 			</div>
 
-			<div class="col-md-6" id="div-nombre-representante">
-				<div class="box box-danger">
-					<div class="box-body">
-					<a href="#" title="Actualizar Representante" class="btn btn-sm btn-default pull-right"><span class="glyphicon glyphicon-refresh"></span></a>
-						<label for="">Representante </label>
-						<p id="p-nombre-representante">@{{ representante.nombre }} @{{ representante.apellido }}</p>
+			<div v-if="estudiante.nombre">
+			<div class="box-header with-border" style="margin-top:-15px">	
+				<h3 class="box-title">Datos Académicos</h3>
+			</div>
+			<div class="box-body">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="">Escolaridad</label>
+							{!! Form::select('escolaridad_id', $escolaridad, NULL, ['class' => 'form-control']) !!}
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="">Mención</label>
+							{!! Form::select('mencion_id', $menciones, NULL, [
+							'class' => 'form-control', 
+							'placeholder' => '', 
+							'required' => 'required', 
+							'id' => 'sel-mencion', 
+							'v-model' => 'mencion_id', 
+							'@change' => 'buscarAno()']
+							) !!}
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="">Ano</label>
+							<select name="ano_id" id="sel_ano" class="form-control" v-model="ano_id" @change='buscarSeccion()'>
+								<option 
+									v-for = "(key, value) in anos" 
+									value = "@{{key}}">
+									@{{ value }}
+								</option>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="">Sección</label>
+							<select name="seccion_id" 
+								id="sel_seccion" 
+								class="form-control" 
+								placeholder="Selccione una seccion" 
+								v-model="seccion_id"
+								>
+								<option 
+									v-for = "(key, value) in secciones" 
+									value = "@{{key}}">
+									@{{ value }}
+								</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
 
-					</div>					
-				</div>
+			<div class="box-footer clearfix">
+				{{ csrf_field() }}
+				<button type="submit" :disabled="formValid()" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> &nbsp;Inscribir Estudiante</button>
 			</div>
-		</div>
+			</form>
+		</div>	
 	</div>
-
-	<div class="box-header with-border" style="margin-top:-15px">	
-		<h3 class="box-title">Datos Académicos</h3>
-	</div>
-	<div class="box-body">
-		<div class="row">
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="">Escolaridad</label>
-					{!! Form::select('escolaridad_id', $escolaridad, NULL, ['class' => 'form-control']) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="">Mención</label>
-					{!! Form::select('mencion_id', $menciones, NULL, [
-					'class' => 'form-control', 
-					'placeholder' => '', 
-					'required' => 'required', 
-					'id' => 'sel-mencion', 
-					'v-model' => 'mencion_id', 
-					'@change' => 'buscarAno()']
-					) !!}
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="">Ano</label>
-					<select name="ano_id" id="sel_ano" class="form-control" v-model="ano_id" @change='buscarSeccion()'>
-						<option 
-							v-for = "(key, value) in anos" 
-							value = "@{{key}}">
-							@{{ value }}
-						</option>
-					</select>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="form-group">
-					<label for="">Sección</label>
-					<select name="seccion_id" 
-						id="sel_seccion" 
-						class="form-control" 
-						placeholder="Selccione una seccion" 
-						v-model="seccion_id"
-						>
-						<option 
-							v-for = "(key, value) in secciones" 
-							value = "@{{key}}">
-							@{{ value }}
-						</option>
-					</select>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="box-footer clearfix">
-		{{ csrf_field() }}
-		<button type="submit" :disabled="formValid()" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> &nbsp;Inscribir Estudiante</button>
-	</div>
-	</form>
-</div>	
 </div>
+
 @endsection
 
 @section('scripts')
@@ -140,6 +151,11 @@
 			error: '',
 			estudiante: { 'cedula': '', nombre: '', apellido: '' },
 			representante: {'nombre': '', apellido: ''}
+		},
+		watch: {
+			'estudiante.cedula': function() {
+				this.estudiante.nombre = '';
+			}
 		},
 		methods: {
 			buscarAno: function ()
@@ -165,6 +181,8 @@
 			},
 			buscarEst: function ()
 			{
+				this.estudiante.nombre = '';
+				this.error = '';
 				this.buscarEstudianteCedula(this.estudiante.cedula).then(function(response)
 				{
 					if(response.data.created){
